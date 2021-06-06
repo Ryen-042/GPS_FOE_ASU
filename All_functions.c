36b696517f7541ade4646793c0546dcc692e8eb1
Abdelrhman-Abdelaziz <85349614+Abdelrhman-Abdelaziz@users.noreply.github.com>
2021-06-06 13:06:38 +0200

    Update All_functions.c

--- All_functions.c
+++ All_functions.c
@@ -99,3 +99,28 @@ uint32_t calc_distance(double lat1, double long1,double lat2, double long2){
 	//return the total integer distance in meter with selling approxmation
 	return((uint32_t)(totaldis+0.5));
 }
+// Get the Latitude and Longitude from the data.
+void getCoor(char data[], double *lat2, double *lon2)
+{
+    char acceptedId[] = "$GPGGA";
+    char messageId[20] = {0};
+    double lat, lon, x;
+    char y;
+
+    sscanf(data, "%[^,],%lf,%lf,%c,%lf", messageId, &x, &lat, &y, &lon);
+
+    // if the message  id isn't accepted.
+    if (strcmp(messageId, acceptedId))
+    {
+        return;
+    }
+
+    *lat2 = ddegree(lat);
+    *lon2 = ddegree(lon);
+}
+// Return the angle in decimal degree.
+double ddegree(double angle)
+{
+	int degree = (int)angle /100;
+	return (degree +  (angle - degree * 100)/60.0);
+}
