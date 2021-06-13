7653a586588a4b694f2060e002652c4aa9825601
EslamMosleh <85323161+EslamMosleh@users.noreply.github.com>
2021-06-13 17:59:37 +0200

    Update 7_Segment.c
    we  get a problem with pins ( D0 &D1 ) with pins of port B so we used pins (E0 & E6)

--- 7_Segment.c
+++ 7_Segment.c
@@ -12,7 +12,7 @@ void PORTABD_Init(void){
 	GPIO_PORTA_AFSEL_R &= ~0xFC; // Disable Serial Pins
 	GPIO_PORTA_DIR_R |= 0xFC; // Set Pins as Output
 	GPIO_PORTA_DEN_R |= 0xFC; // Set Pins as Digital
-	GPIO_PORTA_DATA_R &= 0x00; // Initalize Pins -> All Pins Are Active (For Active Low 7-Segment)
+	//GPIO_PORTA_DATA_R &= 0x00; // Initalize Pins -> All Pins Are Active (For Active Low 7-Segment)
 	
 	// Port (B)
 	GPIO_PORTB_LOCK_R = 0x4C4F434B ;
@@ -22,26 +22,26 @@ void PORTABD_Init(void){
 	GPIO_PORTB_AFSEL_R &= ~0xFF; // Disable Serial Pins
 	GPIO_PORTB_DIR_R |= 0xFF; // Set Pins as Output
 	GPIO_PORTB_DEN_R |= 0xFF; // Set Pins as Digital
-	GPIO_PORTB_DATA_R &= 0x00; // Initalize Pins -> All Pins Are Active (For Active Low 7-Segment)
+	//GPIO_PORTB_DATA_R &= 0x00; // Initalize Pins -> All Pins Are Active (For Active Low 7-Segment)
 	
 	// Port (D)
 	GPIO_PORTD_LOCK_R = 0x4C4F434B ; // Unlock PortD Commit Register
-	GPIO_PORTD_CR_R |= 0xCF; // Allow Changes to PortD
-	GPIO_PORTD_AMSEL_R &= ~0xCF; // Disable Analog Pins 
-	GPIO_PORTD_PCTL_R &= ~0x00FFFFFF; // Disable Serial Pin-Type For Pins
-	GPIO_PORTD_AFSEL_R &= ~0xCF; // Disable Serial Pins
-	GPIO_PORTD_DIR_R |= 0xCF; // Set Pins as Output
-	GPIO_PORTD_DEN_R |= 0xCF; // Set Pins as Digital
-	GPIO_PORTD_DATA_R &= 0x00; // Initalize Pins -> All Pins Are Active (For Active Low 7-Segment)
+	GPIO_PORTD_CR_R |= 0xCC; // Allow Changes to PortD
+	GPIO_PORTD_AMSEL_R &= ~0xCC; // Disable Analog Pins 
+	GPIO_PORTD_PCTL_R &= ~0xFFFFFFFF; // Disable Serial Pin-Type For Pins
+	GPIO_PORTD_AFSEL_R &= ~0xCC; // Disable Serial Pins
+	GPIO_PORTD_DIR_R |= 0xCC; // Set Pins (D2 D3 D6 D7)as Output
+	GPIO_PORTD_DEN_R |= 0xCC; // Set Pins as Digital
+	//GPIO_PORTD_DATA_R &= 0x00; // Initalize Pins -> All Pins Are Active (For Active Low 7-Segment)
 	
 	// port (E)
 	GPIO_PORTE_LOCK_R = 0x4C4F434B;
-	GPIO_PORTE_CR_R = 0x36; // Allow Changes to PortE (1, 2, 4, 5)
-	GPIO_PORTE_AMSEL_R &= ~0x36; // Disable Analog Pins 
-	GPIO_PORTE_PCTL_R &= ~0x00FF0FF0; // Disable Serial Pin-Type For Pins
-	GPIO_PORTE_AFSEL_R &= ~0x36; // Disable Serial Pins
-	GPIO_PORTE_DIR_R |= 0x36; // Set Pins as Output
-	GPIO_PORTE_DEN_R |= 0x36; // Set Pins as Digital
+	GPIO_PORTE_CR_R = 0x3F; // Allow Changes to PortE (0, 1, 2, 4, 5, 6)
+	GPIO_PORTE_AMSEL_R &= ~0x3F; // Disable Analog Pins 
+	GPIO_PORTE_PCTL_R &= ~0x00FFFFFF; // Disable Serial Pin-Type For Pins
+	GPIO_PORTE_AFSEL_R &= ~0x3F; // Disable Serial Pins
+	GPIO_PORTE_DIR_R |= 0x3F; // Set Pins as Output
+	GPIO_PORTE_DEN_R |= 0x3F; // Set Pins as Digital
 }
 
 unsigned int Pins_Of_7Segment(unsigned int c){
@@ -152,19 +152,20 @@ unsigned int PORTA_in_E(unsigned int c){
 unsigned int PORTD_in_E(unsigned int c){
 	unsigned int d;
 	
-	// Missing Pins in PortD -> (4, 5)
+	// Missing Pins in PortD -> (0, 1, 4, 5 )
 	switch (c) {
-		// We will not shift Pins (4, 5) when we Put them in PortE and will Put Them in the same Pins (i.e. Pins (4, 5) of PortE)
+		// We will not shift Pins (0, 4, 5) when we Put them in PortE and will Put Them in the same Pins (i.e. Pins (0, 4, 5) of PortE)
+			// but we need to shift pin 1 to pin 6 in port E 
 		case 0:
 		d = 0x00;    // 1000 1000 (Before Shifting) 
 		break;
 
 		case 1:
-		d = 0x30;    // 1011 1110 (Before Shifting)
+		d = 0x38;    // 1011 1110 (Before Shifting)
 		break;
 
 		case 2:
-		d = 0x10;    // 0001 1001 (Before Shifting)
+		d = 0x11;    // 0001 1001 (Before Shifting)
 		break;
 
 		case 3:
@@ -172,7 +173,7 @@ unsigned int PORTD_in_E(unsigned int c){
 		break;
 
 		case 4:
-		d = 0x20;    // 0010 1110 (Before Shifting)
+		d = 0x28;    // 0010 1110 (Before Shifting)
 		break;
 	
 		case 5:
@@ -197,6 +198,7 @@ unsigned int PORTD_in_E(unsigned int c){
 	}
 	return d;
 }
+
 
 void Set_Pins(unsigned int num){
 	unsigned int Digit_A;
